1dc0a8030a8769eaf5f873688d9458cc3fd6cb27
Eduardo <eduardo@dnf.com.br>
2022-05-11 08:06:33 -0300

    11052022

---
+++ script.js
@@ -0,0 +1,24 @@
+function verificar() {
+    
+    
+    
+    var res = document.querySelector('div#res')
+    var qua = document.getElementsByName('formatoquadro')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+        if(qua[0].checked) {
+            
+                img.setAttribute('src', './img/galeria/030x040 oleo sobre tela_pq.jpg')  // Carregando a imagem
+                res.innerHTML = ('Pintura Oleo sobre tela (030 X 040 cm)')
+        }/*else if (qua[1].checked){
+            
+                img.setAttribute('src', './img/galeria/1.00x070 oleo sob.jpg')
+           
+        }
+        
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`*/
+        res.style.display = 'flex'
+        res.appendChild(img)  // Acrescentou a imagem
+    }
+
