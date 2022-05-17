178dce21805c94d10eb1fff5116f875ce326cb16
Eduardo <eduardo@dnf.com.br>
2022-05-17 14:24:00 -0300

    17052022

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function verificar() {
     img.setAttribute('id', 'foto')
 
         if(qua[0].checked) {
-            
+                
                 img.setAttribute('src', './img/galeria/030x040 oleo sobre tela_pq.jpg')  // Carregando a imagem
                 res.innerHTML = ('Pintura Oleo sobre tela (030 X 040 cm)')
+                
         }/*else if (qua[1].checked){
             
                 img.setAttribute('src', './img/galeria/1.00x070 oleo sob.jpg')
